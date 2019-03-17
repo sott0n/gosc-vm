05e8124f990896c57e3e6b06d60b56131aa594e1
Kohei Yamaguchi <fix7211@gmail.com>
2019-03-17 15:19:26 +0900

    Add main.go and cmd_run.go.

---
+++ cmd_run.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"gosc-vm/compiler"
+	"gosc-vm/cpu"
+	"gosc-vm/lexer"
+	"io/ioutil"
+
+	"github.com/google/subcommands"
+)
+
+type runCmd struct {
+}
+
+//
+// Glue
+//
+func (*runCmd) Name() string     { return "run" }
+func (*runCmd) Synopsis() string { return "Run the given source program." }
+func (*runCmd) Usage() string {
+	return `run :
+	The run sub-command compiles the given source program, and then executes
+	it immediately.
+`
+}
+
+//
+// Flag setup: no flags
+//
+func (p *runCmd) SetFlags(f *flag.FlagSet) {
+}
+
+//
+// Entry-point.
+//
+func (p *runCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	for _, file := range f.Args() {
+		fmt.Printf("Parsing file: %s\n", file)
+
+		// Read the file.
+		input, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Printf("Error reading %s - %s\n", file, err.Error())
+			return subcommands.ExitFailure
+		}
+
+		// Lex it
+		l := lexer.New(string(input))
+
+		// Compile it
+		e := compiler.New(l)
+		e.Compile()
+
+		// Now create a machine to run the compiled program in
+		c := cpu.NewCPU()
+
+		// Load the program
+		c.LoadBytes(e.Output())
+
+		// Run the machine
+		c.Run()
+	}
+	return subcommands.ExitSuccess
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"os"
 
+	"github.com/google/subcommands"
+)
+
+func main() {
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&compileCmd{}, "")
+	subcommands.Register(&executeCmd{}, "")
+	subcommands.Register(&runCmd{}, "")
+
+	flag.Parse()
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
 }
